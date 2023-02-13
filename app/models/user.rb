9f83340f34d4c62c623c394803ed79aa3dd16e68
Patricia Severance <patriciakseverance@gmail.com>
2023-02-13 11:24:37 -0700

    Add validation for password digest, User now has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User <ApplicationRecord 
     validates_presence_of :email, :name 
     validates_uniqueness_of :email
-
+    validates_presence_of :password_digest
+    has_secure_password
 end 
