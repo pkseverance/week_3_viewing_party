9aad581f6f6f82ef882499bdc0c56fc22f6bec70
Patricia Severance <patriciakseverance@gmail.com>
2023-02-13 14:11:47 -0700

    Refactor user create method to validate passwords and implemented sad path error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,21 @@ class UsersController <ApplicationController
         @user = User.find(params[:id])
     end 
 
-    def create 
-        user = User.create(user_params)
-        if user.save
-            redirect_to user_path(user)
-        else  
-            flash[:error] = user.errors.full_messages.to_sentence
+    def create
+        if user_params[:password] == user_params[:password_confirmation]
+            user = User.create(user_params)
+            if user.save
+                redirect_to user_path(user)
+            else  
+                flash[:error] = user.errors.full_messages.to_sentence
+                redirect_to register_path
+            end 
+        else
+            flash[:error] = "Passwords don't match. Are you sure you typed that correctly?"
             redirect_to register_path
-        end 
+        end
     end 
-
+    
     private 
 
     def user_params 
